62d8924330d9f68ea5ef9612c998874bf1fb9408
Max Kolotilin <maxkolmail@gmail.com>
2016-10-08 14:39:55 +0300

    refactoring

--- PhotoAlbum/Scripts/mobile.js
+++ PhotoAlbum/Scripts/mobile.js
@@ -1,78 +1,14 @@
-﻿
-
-var rateModel;
-
-function RateModel(rating) {
-    if (rating.id != 0) {
-        this.method = 'PUT';
-        this.notifyMessage = 'Your rating was updated';
-    } else {
-        this.method = 'POST';
-        this.notifyMessage = 'New rating was accepted';
-        rating.albumId = albumId;
-        rating.applicationUserId = userId;
-    }
-    this.rating = rating;
-};
-
-function rateController(id, rating) {
-    rateModel.rating.rate = rating;
-    $.ajax({
-        type: rateModel.method,
-        url: '/api/Ratings',
-        data: rateModel.rating,
-        success: function (data) {
-            pushNotify('fa fa-star', '<strong> ' + rating + ' stars!</strong>',
-                rateModel.notifyMessage, 3000);
-            if (rateModel.method === 'POST') {
-                rateModel.method = 'PUT';
-                rateModel.notifyMessage = 'Your rating was updated';
-                rateModel.rating = data;
-            }
-        },
-        error: errorHandler
-    });
-}
-
-// init ratebox
-$(function () {
-    $.get({
-        url: '/api/Ratings',
-        data: {
-            albumId: albumId,
-            userId: userId
-        },
-        success: function (rating) {
-            rateModel = new RateModel(rating);
-            $('div.ratebox').attr('data-rating', rating.rate);
-
-            $('.ratebox').raterater({
-                submitFunction: 'rateController',
-                allowChange: true,
-                starWidth: 50,
-                spaceWidth: 5,
-                numStars: 5,
-                step: 0.5
-            });
-        },
-        complete: function () {
-            $('#rating_spinner').hide();
-            $('#rating_block').show();
-        },
-        error: errorHandler
-    });
-});
-
+﻿// required global variables: albumId, userId 
 
 var commentsModel = {
-    myObservableArray: ko.observableArray(),
+    comments: ko.observableArray(),
     isCommentsLoaded: ko.observable(false),
     removeButton: function (comment) {
         $.ajax({
             method: 'DELETE',
             url: '/api/Comments/' + comment.commentId,
             success: function () {
-                commentsModel.myObservableArray.remove(comment);
+                commentsModel.comments.remove(comment);
                 commentsHub.server.deleteComment(comment.commentId);;
             },
             error: errorHandler
@@ -80,7 +16,6 @@ var commentsModel = {
     },
     sendButton: function () {
         var commentHtml = $('#write-comment').summernote('code');
-        $('#write-comment').summernote('code', '');
         if (commentHtml) {
             $.ajax({
                 type: 'POST',
@@ -90,7 +25,8 @@ var commentsModel = {
                     id: albumId
                 },
                 success: function (comment) {
-                    commentsModel.myObservableArray.unshift(comment);
+                    $('#write-comment').summernote('code', '');
+                    commentsModel.comments.unshift(comment);
                     commentsHub.server.addComment(comment.commentId);
                 },
                 error: errorHandler
@@ -109,19 +45,57 @@ var commentsModel = {
         setTimeout(() => { $(element).remove(); $('body').css('overflow', ''); }, 600);
     },
 }
+
+var photoModel = {
+    photos: ko.observableArray(),
+    isPhotosLoaded: ko.observable(false)
+}
+
+ko.applyBindings(photoModel, document.getElementById('photo_container'));
 ko.applyBindings(commentsModel, document.getElementById('comments_block'));
 
 var commentsHub = $.connection.commentsHub;
 
-$(function () {
+commentsHub.client.deleteComment = function (commentId) {
+    commentsModel.comments.remove(function (comment) {
+        return comment.commentId == commentId;
+    });
+}
+
+commentsHub.client.updateLastComment = function (commentId) {
     $.ajax({
         type: 'GET',
         url: '/api/Comments',
-        data: { albumId: albumId, mark: 0 },
+        data: { commentId: commentId },
         dataType: 'json',
+        success: function (comment) {
+            if (comment != null) {
+                commentsModel.comments.unshift(comment);
+            }
+        },
+        error: errorHandler
+    });
+}
+
+$(function () {
+    $.get({
+        url: '/api/Photos',
+        data: { albumId: albumId },
+        success: function (photos) {
+            ko.utils.arrayPushAll(photoModel.photos, photos);
+        },
+        complete: function () {
+            photoModel.isPhotosLoaded(true);
+        },
+        error: errorHandler
+    });
+
+    $.get({
+        url: '/api/Comments',
+        data: { albumId: albumId, mark: 0 },
         success: function (comments) {
             $('#comments').show();
-            ko.utils.arrayPushAll(commentsModel.myObservableArray, comments);
+            ko.utils.arrayPushAll(commentsModel.comments, comments);
         },
         complete: function () {
             commentsModel.isCommentsLoaded(true);
@@ -142,45 +116,4 @@ $(function () {
     $('#send_button_block').show();
 
     $.connection.hub.start();
-
-    commentsHub.client.deleteComment = function (commentId) {
-        commentsModel.myObservableArray.remove(function (comment) {
-            return comment.commentId == commentId;
-        });
-    }
-
-    commentsHub.client.updateLastComment = function (commentId) {
-        $.ajax({
-            type: 'GET',
-            url: '/api/Comments',
-            data: { "commentId": commentId },
-            dataType: 'json',
-            success: function (comment) {
-                if (comment != null) {
-                    commentsModel.myObservableArray.unshift(comment);
-                }
-            },
-            error: errorHandler
-        });
-    }
-});
-
-var photoModel = {
-    photos: ko.observableArray(),
-    isPhotosLoaded: ko.observable(false)
-}
-ko.applyBindings(photoModel, document.getElementById('photo_container'));
-
-$(function () {
-    $.get({
-        url: '/api/Photos',
-        data: { albumId: albumId },
-        success: function (photos) {
-            ko.utils.arrayPushAll(photoModel.photos, photos);
-        },
-        complete: function () {
-            photoModel.isPhotosLoaded(true);
-        },
-        error: errorHandler
-    });
 });
---
+++ PhotoAlbum/Scripts/rating.js
@@ -0,0 +1,63 @@
+﻿// required global variables: albumId, userId 
+
+var rateModel;
+
+function RateModel(rating) {
+    if (rating.id != 0) {
+        this.method = 'PUT';
+        this.notifyMessage = 'Your rating was updated';
+    } else {
+        this.method = 'POST';
+        this.notifyMessage = 'New rating was accepted';
+        rating.albumId = albumId;
+        rating.applicationUserId = userId;
+    }
+    this.rating = rating;
+};
+
+function rateController(id, rating) {
+    rateModel.rating.rate = rating;
+    $.ajax({
+        type: rateModel.method,
+        url: '/api/Ratings',
+        data: rateModel.rating,
+        success: function (data) {
+            pushNotify('fa fa-star', '<strong> ' + rating + ' stars!</strong>',
+                rateModel.notifyMessage, 3000);
+            if (rateModel.method === 'POST') {
+                rateModel.method = 'PUT';
+                rateModel.notifyMessage = 'Your rating was updated';
+                rateModel.rating = data;
+            }
+        },
+        error: errorHandler
+    });
+}
+
+$(function () {
+    $.get({
+        url: '/api/Ratings',
+        data: {
+            albumId: albumId,
+            userId: userId
+        },
+        success: function (rating) {
+            rateModel = new RateModel(rating);
+            $('div.ratebox').attr('data-rating', rating.rate);
+
+            $('.ratebox').raterater({
+                submitFunction: 'rateController',
+                allowChange: true,
+                starWidth: 50,
+                spaceWidth: 5,
+                numStars: 5,
+                step: 0.5
+            });
+        },
+        complete: function () {
+            $('#rating_spinner').hide();
+            $('#rating_block').show();
+        },
+        error: errorHandler
+    });
+});
